d77fe81f4e549e4bd1cc06bb0c3e55838afb9a1d
ko <ko.liu.learn@gmail.com>
2018-05-04 23:38:30 +0800

    Add configs

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "env": {
+    "browser": true,
+    "node": true,
+    "es6": true,
+  },
+  "rules": {
+    "linebreak-style": ["error", "windows"] // "unix"
+  }
+};
